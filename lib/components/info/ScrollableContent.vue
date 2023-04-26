ffbc5a428d2f64350a5eb7609809335c4248474c
Andrés Brugarolas <Brugarolas@users.noreply.github.com>
2023-04-26 21:20:00 +0200

    Fix ScrollableContent & Parent

--- lib/components/info/ScrollableContent.vue
+++ lib/components/info/ScrollableContent.vue
@@ -4,9 +4,9 @@
 </template>
 
 <style lang="scss">
-.scrolling-content {
+.scrollable-content {
   /* https://dev.to/peiche/100vh-behavior-on-chrome-2hm8 */
-  max-height: calc(100vh - 56px);
+  max-height: 100vh;
   flex: 1 1 0;
   overflow-y: auto;
   overflow-x: hidden;
--- lib/components/info/ScrollableParent.vue
+++ lib/components/info/ScrollableParent.vue
@@ -4,9 +4,9 @@
 </template>
 
 <style lang="scss">
-.scrolling-parent {
+.scrollable-parent {
   /* https://dev.to/peiche/100vh-behavior-on-chrome-2hm8 */
-  min-height: calc(100vh - 56px);
+  min-height: 100vh;
   min-height: -webkit-fill-available;
   flex: 1 1 0;
   display: flex;
